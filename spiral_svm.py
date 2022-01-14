1cfab5b0c3398b7c2b13ba2dc6c9ed71de8c4db9
siddu6003 <51458421+siddu6003@users.noreply.github.com>
2022-01-14 17:27:45 +0530

    Update spiral_svm.py

--- spiral_svm.py
+++ spiral_svm.py
@@ -19,7 +19,7 @@ with open('model_spiral','rb') as f:
 print("Model loaded")
 print("\n")
 path=os.getcwd()
-url='app'+'//uploads//image.png'
+url='app'+'/uploads/image.png'
 print(url)
 img=imread(url)
 img_resize=resize(img,(150,150,3))
